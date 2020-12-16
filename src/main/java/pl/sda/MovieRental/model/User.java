dc782a0f8d5b16e6d926069188e1af45d937eff4
lukas <lukasz.grazda@gmail.com>
2020-12-16 12:08:07 +0100

    add RegistrationController

--- src/main/java/pl/sda/MovieRental/model/User.java
+++ src/main/java/pl/sda/MovieRental/model/User.java
@@ -17,7 +17,7 @@ import java.util.Objects;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
