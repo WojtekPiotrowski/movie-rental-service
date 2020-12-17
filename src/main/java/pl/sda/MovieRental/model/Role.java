7380342ed0d12067b163d19ac0a0e1bff36fbdc3
lukas <lukasz.grazda@gmail.com>
2020-12-17 12:36:15 +0100

    add Role Class, entity and user rold in import.sql

---
+++ src/main/java/pl/sda/MovieRental/model/Role.java
@@ -0,0 +1,22 @@
+package pl.sda.MovieRental.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+}
